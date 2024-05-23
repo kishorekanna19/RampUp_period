b7d00a7fa4b85fd76654552b9aaaecef37c2aa24
kishore <kishorekannadgl@gmail.com>
2024-05-23 12:43:45 +0530

    list file is uploaded

--- java_ramp_up/queue.java
+++ java_ramp_up/queue.java
@@ -13,16 +13,18 @@ public class queue {
         System.out.println(qu);
 
         // remove- it remove the last inserted
-
-        System.out.println(qu.remove());
+        System.out.println("Remove method: " + qu.remove());
 
         // element() - it will returns the head element
-
-        System.out.println(qu.element());
+        System.out.println("Element method(): " + qu.element());
 
         // poll() - will return and remove the head of the queue
+        System.out.println("Poll method:" + qu.poll());
 
-        System.out.println(qu.poll());
+        // peek() - same as element but if queue is empty it will return null value
+        System.out.println(qu);
+
+        System.out.println("Peek Method(): " + qu.peek());
 
     }
 
